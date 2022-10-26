4375a6ec43c1b80180ec49565abe8fc1fa544bf9
Данил Березенцев <danil.ber@mail.ru>
2022-10-27 02:13:52 +0300

    add functions and tests
    Добавил функции перевода из матрицы переходов конечного автомата в регулярное выражение

--- lib/GEMEMDB.rb
+++ lib/GEMEMDB.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 require_relative "GEMEMDB/version"
+require 'matrix'
 
 module GEMEMDB
   class Error < StandardError; end
+  class InvalidMatrix < RuntimeError; end
 
 	class Tafya
+    # Создание конечного автомата из регулярного выражения
 		def reg_fm(s_param)
 			s = s_param.delete(' ')
 			eps = "Eps"
@@ -83,5 +86,190 @@ module GEMEMDB
 
 			return m
 		end
+
+    # Создание уравнения из матрицы переходов конечного автомата
+    def matrixToEquation(param, perem, matrix)
+      # Создание уравнения из матрицы
+      equation = Array.new(perem.count) {''}
+      (0..perem.count-1).to_a.each do |i|
+          (0..param.count-1).to_a.each do |j|
+            if matrix[i,j] != '0' && matrix[i,j] != '-'
+              if matrix[i,j].size > 1
+                (0..matrix[i,j].size-1).to_a.each do |k|
+                  equation[i] += param[j].to_s + matrix[i,j][k].chr.to_s + '+'
+                end
+              else
+                equation[i] += param[j].to_s + matrix[i,j].to_s + '+'
+              end
+            end
+          end
+          equation[i].chop!
+      end
+      (0..perem.count-1).to_a.each do |i|
+        if perem[i][1] == 'e'
+          equation[i] += '+' + 'e'
+        end
+      end
+      (0..perem.count-1).to_a.each do |i|
+        (0..perem.count-1).to_a.each do |j|
+          if (equation[i].include? perem[j][0] and i>j)
+            throw InvalidMatrix
+          end
+        end
+      end
+      equationToSolution(perem, equation)
+    end
+
+    # Сортировка уравнения к виду (x1 = x1 + x2 + ... + xn, x2 = x2 + x1 + ... + xn, ... , xn = xn + x1 + x2 + ... + xn-1)
+    private def sortEquation(perem, equation)
+      # Сортировка уравнения
+      newEquation = Array.new(perem.count) {''}
+      z = Hash.new()
+      (1..perem.count).to_a.each do |j|
+        z[perem[j-1][0]] = (j+1).to_s
+      end
+      (0..perem.count-1).to_a.each do |i|
+        (1..perem.count).to_a.each do |k|
+          z[perem[k-1][0]] = (z[perem[k-1][0]].to_i - 1).to_s
+          if z[perem[k-1][0]] == '0'
+            z[perem[k-1][0]] = perem.count.to_s
+          end
+        end
+        (0..perem.count-1).to_a.each do |k|
+          equation[i].gsub!(perem[k][0], 'X' + z[perem[k][0]])
+        end
+
+        help = equation[i].split('+')
+        help.sort_by! {|a| a[-1]}
+        (0..perem.count-1).to_a.each do |k|
+          help.each {|a| a.gsub!('X' + z[perem[k][0]], perem[k][0])}
+        end
+
+        (0..help.count-1).to_a.each do |j|
+          if help[j].match('\d*[A-Z]\d*') != nil
+            newEquation[i] += help[j] + '+'
+          end
+        end
+
+        (0..help.count-1).to_a.each do |j|
+          if help[j].match('^[^A-Z]*$') != nil
+            newEquation[i] += help[j] + '+'
+          end
+        end
+        newEquation[i].chop!
+      end
+      newEquation
+    end
+
+    # Поиск решения каждого из уравнений
+    private def equationToPredSolution(perem, equation)
+      h = Hash.new()
+      # Приведение к решению уравнения
+      (0..perem.count-1).to_a.each do |i|
+        h[perem[i][0]] = equation[i]
+        if h[perem[i][0]].include? perem[i][0]
+          help = h[perem[i][0]].split('+')
+          p = ''
+          newStr = ''
+          (0..help.count-1).to_a.each do |j|
+            if help[j].include? perem[i][0]
+              p += help[j].chop + '+'
+            end
+          end
+          if p.size == 2
+            newStr += p.chop + '*'
+          else
+            newStr += '(' + p.chop + ')*'
+          end
+          p = ''
+          (0..help.count-1).to_a.each do |j|
+            if !help[j].include? perem[i][0]
+              p += help[j]+ '+'
+            end
+          end
+          p.chop!
+          if !p.include? '+'
+            newStr += p
+          else
+            newStr += '(' + p + ')'
+          end
+          h[perem[i][0]] = newStr
+          h[perem[i][0]].gsub!('+)', ')')
+          h[perem[i][0]].gsub!('()', '')
+          h[perem[i][0]].gsub!('(+', '(')
+          h[perem[i][0]].gsub!('(e)', '')
+          h[perem[i][0]].gsub!('*e', '*')
+          h[perem[i][0]].gsub!('(\d)', '\d')
+          # h[perem[i][0]].gsub!('(())', '()') # замена 2 скобок на одну
+          # TODO Сделать удаление лишних скобок
+        end
+      end
+      h
+    end
+
+    # Приведение отдельной строки к решению
+    private def stringToPredSolution(perem, str)
+      # Приведение к решению уравнения
+      if str.include? perem
+        help = str.split('+')
+        p = ''
+        newStr = ''
+        (0..help.count-1).to_a.each do |j|
+          if help[j].include? perem
+            p += help[j].chop + '+'
+          end
+        end
+        if p.size == 2
+          newStr += p.chop + '*'
+        else
+          newStr += '(' + p.chop + ')*'
+        end
+        p = ''
+        (0..help.count-1).to_a.each do |j|
+          if !help[j].include? perem
+            p += help[j]+ '+'
+          end
+        end
+        p.chop!
+        if !p.include? '+'
+          newStr += p
+        else
+          newStr += '(' + p + ')'
+        end
+        newStr.gsub!('+)', ')')
+        newStr.gsub!('()', '')
+        newStr.gsub!('(+', '(')
+        newStr.gsub!('(e)', '')
+        newStr.gsub!('*e', '*')
+        # h[perem[i][0]].gsub!('(())', '()') # замена 2 скобок на одну
+        # TODO Сделать удаление лишних скобок
+      end
+      newStr
+    end
+
+    # Решение уравнения(поиск регулярного выражения)
+    def equationToSolution(perem, equation)
+      # Решение уравнения
+      equation = sortEquation(perem, equation)
+      h = equationToPredSolution(perem, equation)
+      # Подстановка решений
+      (0..perem.count-1).to_a.reverse.each do |i|
+        (0..perem.count-1).to_a.each do |j|
+          if equation[i].include? perem[j][0] and perem[j][0] != perem[i][0]
+            if equation[j][-1] != ')' and equation[j][-1] != '*'
+              equation[i].gsub!(perem[j][0], '(' + h[perem[j][0]] + ')')
+            else
+              equation[i].gsub!(perem[j][0], h[perem[j][0]])
+            end
+          end
+        end
+        if equation[i].include? perem[i][0]
+          equation[i] = stringToPredSolution(perem[i][0], equation[i])
+          h[perem[i][0]] = equation[i]
+        end
+      end
+      h[perem[0][0]]
+    end
+
 	end
 end
--- test/test_GEMEMDB.rb
+++ test/test_GEMEMDB.rb
@@ -37,4 +37,71 @@ class TestGEMEMDB < Minitest::Test
       object.reg_fm('(x + y) * d (z + y)'), 'fifth_complex'
   end
 
+  def test_matr_with_all_e
+    object = GEMEMDB::Tafya.new()
+    assert_equal('1*(00*(1(1(1+0)*+e)+e)+e)', object.matrixToEquation(['0', '1'], ['Se', 'De', 'Pe', 'Qe'], Matrix[['D', 'S'], ['D', 'P'], ['0', 'Q'], ['Q', 'Q']]))
+  end
+
+  def test_matr_with_zero_dash
+    object = GEMEMDB::Tafya.new()
+    assert_equal('2*(10*20*+10*+e)', object.matrixToEquation(['0', '1', '2'], ['Ae', 'B', 'Ce'], Matrix[['0', 'BC', 'A'], ['B', '-', 'C'], ['C', '0', '-']]))
+  end
+
+  def test_matr_with_one_transition
+    object = GEMEMDB::Tafya.new()
+    assert_equal('2*', object.matrixToEquation(['0', '1', '2'], ['Ae'], Matrix[['0', '0', 'A']]))
+  end
+
+  def test_matr_with_branching
+    object = GEMEMDB::Tafya.new()
+    assert_equal('(1+0)*(01(1+0)*)', object.matrixToEquation(['0', '1'], ['A', 'B', 'Ce'], Matrix[['AB', 'A'], ['0', 'C'], ['C', 'C']]))
+  end
+
+  def test_sort_equation
+    object = GEMEMDB::Tafya.new()
+    assert_equal(['01*01*2C+2C+01*'], object.sortEquation(['C'], ["2C+01*+01*01*2C"]))
+  end
+
+  def test_equation_to_solution
+    object = GEMEMDB::Tafya.new()
+    assert_equal('(01*01*2+2)*01*', object.equationToSolution(['C'], ["2C+01*+01*01*2C"]))
+  end
+
+  def test_equation_to_one_pred_solution
+    object = GEMEMDB::Tafya.new()
+    assert_equal({"C"=>"0*"}, object.equationToPredSolution(['C'], ["0C+e"]))
+  end
+
+  def test_equation_to_pred_solution
+    object = GEMEMDB::Tafya.new()
+    assert_equal({"C"=>"(2+01*01*2)*01*"}, object.equationToPredSolution(['C'], ["2C+01*+01*01*2C"]))
+  end
+
+  def test_equation_to_two_pred_solution
+    object = GEMEMDB::Tafya.new()
+    assert_equal({"B"=>"0*0C", "C"=>"(0+1)*2B"}, object.equationToPredSolution(['B', 'C'], ["0B+0C", "0C+1C+2B"]))
+  end
+
+  def test_string_to_pred_solution
+    object = GEMEMDB::Tafya.new()
+    assert_equal('(1+0)*', object.stringToPredSolution('Q', "1Q+0Q+e"))
+  end
+
+  def test_string_to_pred_solution_nil
+    object = GEMEMDB::Tafya.new()
+    assert_equal(nil, object.stringToPredSolution('P', "1Q+0Q+e"))
+  end
+
+  def test_invalid_matrix1
+    object = GEMEMDB::Tafya.new()
+    assert_throw(InvalidMatrix) do object.matrixToEquation(['0', '1'], ['Ae', 'B', 'C'], Matrix[['B', 'A'], ['C', 'B'], ['A', 'C']])
+    end
+  end
+
+  def test_invalid_matrix2
+    object = GEMEMDB::Tafya.new()
+    assert_throw(InvalidMatrix) do object.matrixToEquation(['0', '1', '2'], ['A', 'Be', 'Ce'], Matrix[['A', 'A', 'B'], ['0', 'C', 'B'], ['0', '0', 'B']])
+    end
+  end
+
 end
