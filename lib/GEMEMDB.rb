118980545925d0e9f531c2b02adb7115ce5c2b1f
EgorCita <murashkoegor@yandex.ru>
2022-10-26 20:11:20 +0300

    Попытка коммита

---
+++ lib/GEMEMDB.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "GEMEMDB/version"
+
+module GEMEMDB
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/GEMEMDB/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module GEMEMDB
+  VERSION = "0.1.0"
+end
