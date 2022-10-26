33390e907bb1c343afd4b21f3f63964977a654c8
EgorCita <73609503+EgorCita@users.noreply.github.com>
2022-10-26 21:24:17 +0300

    Тестовая попытка теста
    Попытка добавить тесты для гема

--- test/test_GEMEMDB.rb
+++ test/test_GEMEMDB.rb
@@ -10,4 +10,15 @@ class TestGEMEMDB < Minitest::Test
   def test_it_does_something_useful
     assert false
   end
+  
+  def test1
+		assert_equal true, [[{"x"=>1}, {"y"=>2}, {"z"=>3}], [{"Eps"=>4}], [{"Eps"=>4}], [{"Eps"=>4}]] ==
+		GEMEMDB::reg_fm('x + y + z'), 'test1'
+	end
+
+	def test2
+		assert_equal true, [[{"Eps"=>1}], [{"x"=>2}, {"y"=>3}, {"d"=>4}], [{"Eps"=>1}], [{"Eps"=>1}], [{"Eps"=>5}], [{"z"=>6}, {"y"=>7}], [{"Eps"=>8}], [{"Eps"=>8}]] ==
+		GEMEMDB::reg_fm('(x + y) * d (z + y)'), 'test2'
+	end
+  
 end
